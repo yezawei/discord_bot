4d11600501f14036531d94a54e10276d165221e7
yezawei <112426848+yezawei@users.noreply.github.com>
2023-03-22 20:21:06 -0500

    Update
    added leave and join vc commands for bot

--- main.py
+++ main.py
@@ -71,7 +71,22 @@ async def on_member_remove(member):
     await channel.send(json.loads(response.text)['content'])
 
     
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not in a voice channel, you must be in a voice channel to run this command.")
 
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I left the voice channel")
+    else:
+        await ctx.send("I am not in a voice channel")
 
 client.run(BOTTOKEN)
 
